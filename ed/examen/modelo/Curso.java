29289c8f4fdd96bd75261d1f3c5f0f177aa49d8a
Alejandro <daw01.2021@gmail.com>
2021-02-23 23:14:21 +0100

    Documentada la clase curso

--- ed/examen/modelo/Curso.java
+++ ed/examen/modelo/Curso.java
@@ -2,14 +2,24 @@ package ed.examen.modelo;
 
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * 
+ * @author Alejandro Agudelo
+ * version 1.0.0
+ *
+ */
 public class Curso {
 	
 	private List<Persona> listaAlumnos;
 
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**
+	 * 
+	 * Metodo que sirve para eliminar a un alumno
+	 * 
+	 * @param dni documento del alumno que se desea eliminar
+	 * @throws Exception Excepcion lanzada si la longitud del dni es mayor o menor de 9 caracteres
+	 * 
+	 */
 	public void eliminarAlumno(String dni) throws Exception {
 		if(dni.length()==9) {//comprobar la longitud del dni
 			listaAlumnos.remove(new Persona(dni, "", "")); //solo hace falta el dni
@@ -18,16 +28,23 @@ public class Curso {
 		}
 	}
 	
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**
+	 * 
+	 * Metodo que sirve para añadir a un alumno
+	 * 
+	 * @param p Persona que queremos añadir a la lista de alumnos de un curso
+	 */
 	public void aniadirAlumno(Persona p) {
 		listaAlumnos.add(p);
 	}
 	
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**
+	 * 
+	 * Metodo que sirve para comprobar si un alumno está reegsitrado
+	 * 
+	 * @param dni Documento del alumno que queremos comprobar si esta registrado en la lista de alumnos
+	 * @return devuelve true si el alumno está registrado y false si no lo está
+	 */
 	public Boolean estaRegistrado(String dni) {
 		int i =0;
 		Boolean encontrado=false;
@@ -40,16 +57,20 @@ public class Curso {
 		return encontrado;
 	}
 	
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**
+	 * 
+	 * Metodo que añade una lista de personas a un curso
+	 * 
+	 */
 	public Curso() {
 		listaAlumnos= new ArrayList<Persona>();
 	}
 	
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**
+	 * 
+	 * Metodo que nos muestr cuantos alumnos hay en un curso
+	 * @return devuelve el numero de alumnos que hay en un curso
+	 */
 	public Integer numeroAlumnos() {return listaAlumnos.size();}
 	
 	
